fe38f2303875021a9909289ad60df50552e7012d
Kanade Ina <kana60919@gmail.com>
2019-01-05 19:13:58 +0900

    데이터베이스 및 테이블 생성 코드 제거

--- www/js/memo.create.js
+++ www/js/memo.create.js
@@ -24,44 +24,7 @@ $(document).ready(function () {
     createSheduleTable();
 });
 
-// 데이터베이스 생성 및 오픈
-function openDB() {
-    db = window.openDatabase('SubjectDB', '1.0', 'SubjectDB', 1024 * 1024);
-    console.log('1_DB 생성...');
-}
 
-function createPrivateTable() {
-
-    db.transaction(function (tr) {
-        var createSQL = 'create table if not exists Private_Storage(subject_name text, notes text, photo blob )';
-        tr.executeSql(createSQL, [], function () {
-            console.log('Private_Storage_테이블생성_sql 실행 성공...');
-        }, function () {
-            console.log('Private_Storage_테이블생성_sql 실행 실패...');
-        });
-    }, function () {
-        console.log('Private_Storage_테이블 생성 트랜잭션 실패...롤백은 자동');
-    }, function () {
-        console.log('Private_Storage_테이블 생성 트랜잭션 성공...');
-    });
-
-}
-
-function createSheduleTable(){
-
-    db.transaction(function (tr) {
-        var createSQL = 'create table if not exists Schedule(week_code integer, subject_code text, time_code text)';
-        tr.executeSql(createSQL, [], function () {
-            console.log('Schedule_테이블생성_sql 실행 성공...');
-        }, function () {
-            console.log('Schedule1_테이블생성_sql 실행 실패...');
-        });
-    }, function () {
-        console.log('Schedule_테이블 생성 트랜잭션 실패...롤백은 자동');
-    }, function () {
-        console.log('Schedule_테이블 생성 트랜잭션 성공...');
-    });
-}
 
 function selectSuject_Name() {
     db.transaction(function (tr) {
