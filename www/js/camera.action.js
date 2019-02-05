dfac70884a820feea3a865d4ba53248e30a80add
Kanade Ina <kana60919@gmail.com>
2019-02-05 13:17:00 +0900

    Add : Camera function

--- www/js/camera.action.js
+++ www/js/camera.action.js
@@ -1,3 +1,19 @@
 myApp.onPageInit("Camera", function (page) {
-    
-});
+    document.getElementById("Take_Picture").addEventListener("click", TakePicture); 
+
+    function TakePicture() { 
+        navigator.camera.getPicture(onSuccess, onFail, {  
+           quality: 50, 
+           destinationType: Camera.DestinationType.DATA_URL 
+        });  
+        
+        function onSuccess(imageData) { 
+           var image = document.getElementById('myImage'); 
+           image.src = "data:image/jpeg;base64," + imageData; 
+        }  
+        
+        function onFail(message) { 
+           alert('Failed because: ' + message); 
+        } 
+     }
+}); 
