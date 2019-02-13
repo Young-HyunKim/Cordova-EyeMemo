fc6cb9e32305002aadd35cdfeedc1a6470126c75
Young-Hyun Kim <contact@younghyun.kr>
2019-02-14 04:20:09 +0900

    Change : <input> Id

--- www/js/memo.create.js
+++ www/js/memo.create.js
@@ -5,12 +5,13 @@ myApp.onPageInit("Note", function (page) {
 
     $$('#note_save').on('click', function () {
         
-        Note_Content = $$("keyword").val();
-        console.log("Note_Content : "+Note_Content)       
+        Note_Content = $$("#content").val();
+        console.log("Note_Content : "+Note_Content);
+
         var CS = localStorage.getItem("T_Subject_Name[" + NowTime.getDay() + "][" + (NowTime.getHours() - 9) + "]");
         
         if(CS == ""){
-            localStorage.setItem(Note_Default,Note_Content);
+            localStorage.setItem("수업 이외",Note_Content);
         }else{
             localStorage.setItem(CS,Note_Content);
         }
