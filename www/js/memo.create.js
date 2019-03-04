8c5c1b907ed11d482d6b9cb6c383c3f628d7201e
Young-Hyun Kim <contact@younghyun.kr>
2019-03-05 01:48:52 +0900

    Add : Memo Multi Save

--- www/js/memo.create.js
+++ www/js/memo.create.js
@@ -1,11 +1,13 @@
 var Note_Content = "";
 var Note_Default = "수업 이외";
+var Note_Numbering = -1;
 
 myApp.onPageInit("Note", function (page) {
-    var Note_Numbering = 0;
+    
     localStorage.setItem("Note_Numbering",Note_Numbering);
 
     $$('#note_save').on('click', function () {
+        console.log(Note_Numbering);
         
         Note_Content = $$("#content").val();
         console.log("Note_Content : "+Note_Content);
@@ -14,10 +16,14 @@ myApp.onPageInit("Note", function (page) {
         
         if(CS == "" || CS == null){
             Note_Numbering += 1;
+            localStorage.setItem("Note_Numbering",Note_Numbering);
             localStorage.setItem("수업 이외["+ Note_Numbering +"]",Note_Content);
+            console.log("TURE : "+Note_Numbering);
         }else{
             Note_Numbering += 1;
+            localStorage.setItem("Note_Numbering",Note_Numbering);
             localStorage.setItem(CS+"["+ Note_Numbering +"]",Note_Content);
+            console.log("FALSE : "+Note_Numbering);
         }
         
         mainView.router.loadPage('home.page.html');
