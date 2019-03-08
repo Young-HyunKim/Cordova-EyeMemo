5d21344cc18900c5b6e153072986aa77cd461560
Young-Hyun Kim <contact@younghyun.kr>
2019-03-09 06:35:01 +0900

    Change : Multi Save Logic

--- www/js/memo.create.js
+++ www/js/memo.create.js
@@ -1,10 +1,22 @@
 var Note_Content = "";
 var Note_Default = "수업 이외";
-var Note_Numbering = -1;
+var Note_Numbering = 0;
 
 myApp.onPageInit("Note", function (page) {
     
-    localStorage.setItem("Note_Numbering",Note_Numbering);
+    $(document).ready(function () {
+        
+        if(localStorage.getItem("Note_Numbering") == null){
+            Note_Numbering = 0;
+            localStorage.setItem("Note_Numbering",Note_Numbering);
+        }else{
+            Note_Numbering = parseInt(localStorage.getItem("Note_Numbering"));
+
+        }
+
+    });
+    
+
 
     $$('#note_save').on('click', function () {
         console.log(Note_Numbering);
@@ -16,11 +28,13 @@ myApp.onPageInit("Note", function (page) {
         
         if(CS == "" || CS == null){
             Note_Numbering += 1;
+
             localStorage.setItem("Note_Numbering",Note_Numbering);
             localStorage.setItem("수업 이외["+ Note_Numbering +"]",Note_Content);
             console.log("TURE : "+Note_Numbering);
         }else{
             Note_Numbering += 1;
+
             localStorage.setItem("Note_Numbering",Note_Numbering);
             localStorage.setItem(CS+"["+ Note_Numbering +"]",Note_Content);
             console.log("FALSE : "+Note_Numbering);
